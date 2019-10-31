f0ec67f2905eb5014dfab286064f9a578c05f193
Leo Singer <leo.singer@ligo.org>
2019-10-31 11:00:23 -0400

    Streamline numpy adapter code

--- example/utils/numpy_adapters.py
+++ example/utils/numpy_adapters.py
@@ -1,5 +1,21 @@
+"""Adapt Numpy values for postgres databases.
+
+Inspired by https://github.com/kgullikson88/Stellar_database/blob/master/NumpyAdaptorsPostgreSQL.py.
+"""
 import numpy as np
 from psycopg2.extensions import register_adapter, AsIs
 
-register_adapter(np.float64, AsIs)
-register_adapter(np.int64, AsIs)
+
+def adapt_literal(const):
+    return lambda arg: const
+
+
+for dtype in [np.int8, np.int16, np.int32, np.int64,
+              np.uint8, np.uint16, np.uint32, np.uint64,
+              np.float32, np.float64]:
+    register_adapter(dtype, AsIs)
+
+register_adapter(np.nan, lambda value: "'NaN'")
+register_adapter(np.inf, lambda value: "'Infinity'")
+register_adapter(-np.inf, lambda value: "'-Infinity'")
+register_adapter(np.ndarray, lambda value: AsIs(value.tolist()))
