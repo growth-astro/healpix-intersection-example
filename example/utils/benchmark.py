941c3e4296c5a7f8d72f3054e966e09badbdc72d
Leo Singer <leo.singer@ligo.org>
2020-02-18 23:33:10 -0500

    Show milliseconds

--- example/utils/benchmark.py
+++ example/utils/benchmark.py
@@ -8,5 +8,5 @@ def time(label):
     start = perf_counter()
     yield
     end = perf_counter()
-    elapsed = np.format_float_positional(end - start, 1, trim='0')
+    elapsed = np.format_float_positional(end - start, 3, trim='0')
     print('***', label, '-', elapsed, 's')
